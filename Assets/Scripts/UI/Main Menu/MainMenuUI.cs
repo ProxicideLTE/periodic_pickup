db6a3dbb705d5ef5cf8a781ed5ba05982a32d645
ProxicideLTE <jayvin.duong@gmail.com>
2013-09-16 16:19:08 -0400

    UI Aspect Ratios

--- Assets/Scripts/UI/Main Menu/MainMenuUI.cs
+++ Assets/Scripts/UI/Main Menu/MainMenuUI.cs
@@ -5,12 +5,26 @@ public class MainMenuUI : MonoBehaviour {
 	
 	// Instance variables.
 	public tk2dUIItem			btn_start;
+	public tk2dUIItem			btn_instructions;
+	
+	public GameObject			instructions_menu;
+	
+	private bool				toggle_instructions;
+	
+	/// <summary>
+	/// Start this instance.
+	/// </summary>
+	void Start() {
+		this.toggle_instructions = false;
+		this.instructions_menu.SetActive(false);
+	}
 	
 	/// <summary>
 	/// Raises the enable event.
 	/// </summary>
 	void OnEnable() {
 		this.btn_start.OnClick += onStartGameClick;
+		this.btn_instructions.OnClick += onInstructionsClick;
 	}
 	
 	/// <summary>
@@ -21,10 +35,26 @@ public class MainMenuUI : MonoBehaviour {
 	}
 	
 	/// <summary>
+	/// On the instructions click.
+	/// </summary>
+	void onInstructionsClick() {
+		if (!this.toggle_instructions) {
+			this.instructions_menu.SetActive(true);
+			this.toggle_instructions = true;	
+		}
+		
+		else {
+			this.instructions_menu.SetActive(false);
+			this.toggle_instructions = false;
+		}
+	}
+	
+	/// <summary>
 	/// Raises the disable event.
 	/// </summary>
 	void OnDisable() {
 		this.btn_start.OnClick -= onStartGameClick;
+		this.btn_instructions.OnClick += onInstructionsClick;
 	}		
 	
 }
